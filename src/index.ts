2847d3623d67431daab78c6a7da9aed022eb7dac
Jonathan West <jonathanrgwest@gmail.com>
2022-11-06 16:09:20 -0500

    working pin button for each cell, to add to selected column in notebook

--- src/index.ts
+++ src/index.ts
@@ -8,30 +8,218 @@ import {
 import { ToolbarButton } from '@jupyterlab/apputils';
 
 import { DocumentRegistry } from '@jupyterlab/docregistry';
+// import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
 
 import {
   NotebookPanel,
   INotebookModel,
 } from '@jupyterlab/notebook';
 
+// From runbtn ext
+import * as React from 'react';
+import { ReactWidget } from '@jupyterlab/apputils';
 import {
-  NBTestUtils
-} from '@jupyterlab/testutils';
+  INotebookTracker,
+  NotebookActions
+} from '@jupyterlab/notebook';
 
-import $ from 'jquery';
+import { ICellFooter, Cell } from '@jupyterlab/cells';
 
-const rendermime = NBTestUtils.defaultRenderMime();
+import { ReadonlyPartialJSONObject } from '@lumino/coreutils';
 
-import { CodeCell, CodeCellModel} from '@jupyterlab/cells';
+import { CommandRegistry } from '@lumino/commands';
+
+import { IEditorServices } from '@jupyterlab/codeeditor';
+import '../style/index.css';
+
+// import {
+//   NBTestUtils
+// } from '@jupyterlab/testutils';
+
+// import $ from '@types/jquery';
+
+// const rendermime = NBTestUtils.defaultRenderMime();
+
+// import { CodeCell, CodeCellModel }e from '@jupyterlab/cells';
+
+/**
+ * The CSS classes added to the cell footer.
+ */
+ const CELL_FOOTER_CLASS = 'jp-CellFooter';
+ const CELL_FOOTER_DIV_CLASS = 'ccb-cellFooterContainer';
+ const CELL_FOOTER_BUTTON_CLASS = 'ccb-cellFooterBtn';
+ 
+ function activateCommands(
+   app: JupyterFrontEnd,
+   tracker: INotebookTracker
+ ): Promise<void> {
+   // tslint:disable-next-line:no-console
+   console.log('JupyterLab extension jupyterlab-cellcodebtn is activated!');
+ 
+   Promise.all([app.restored]).then(([params]) => {
+     const { commands, shell } = app;
+ 
+     function getCurrent(args: ReadonlyPartialJSONObject): NotebookPanel | null {
+       const widget = tracker.currentWidget;
+       const activate = args.activate !== false;
+ 
+       if (activate && widget) {
+         shell.activateById(widget.id);
+       }
+       var columns: HTMLCollectionOf<Element> = document.getElementsByClassName("column");
+       var notebook: HTMLCollectionOf<Element> = document.getElementsByClassName("jp-Notebook");
+      if(notebook.length == 0) {
+        // Upon initial call, the jp-Notebook element likely has not loaded yet
+        console.log("Error: no jp-Notebook class elements found");
+        console.log(notebook);
+      } else {
+        // For now, assume one column, and we are completing initial page setup
+        // var columns = Array.from(document.getElementsByClassName("column") as HTMLCollectionOf<HTMLElement>)
+        nCols = columns.length;
+        var colIdx = 0;
+        // Find selected column
+        for(var c = 0; c<columns.length; c++){
+            if(columns[c].classList.contains("selected")) {
+                colIdx = c;
+                break;
+            }
+        }
+        // figure out which cell actually was clicked, and add it to selected col for first col if none selected
+        //https://jupyterlab.readthedocs.io/en/3.3.x/api/interfaces/notebook.inotebooktracker.html#notes
+        var jp_cells = (notebook[0] as HTMLElement).getElementsByClassName("jp-Cell");
+        console.log(jp_cells.length);
+        for(var i = 0; i < jp_cells.length; i++) {
+          if(jp_cells[i].classList.contains("jp-mod-selected")) {
+            columns[colIdx].append(jp_cells[i]);
+            console.log("append attempted: ", jp_cells[i]);
+            break;
+          }
+        }
+      }
+      return widget;
+    }
+ 
+     function isEnabled(): boolean {
+       return (
+         tracker.currentWidget !== null &&
+         tracker.currentWidget === app.shell.currentWidget
+       );
+     }
+ 
+     commands.addCommand('run-selected-codecell', {
+       label: 'Run Cell',
+       execute: args => {
+         const current = getCurrent(args);
+ 
+         if (current) {
+           const { context, content } = current;
+           NotebookActions.run(content, context.sessionContext);
+           // current.content.mode = 'edit';
+         }
+       },
+       isEnabled
+     });
+   });
+ 
+   return Promise.resolve();
+ }
+ 
+ /**
+  * Extend the default implementation of an `IContentFactory`.
+  */
+ export class ContentFactoryWithFooterButton extends NotebookPanel.ContentFactory {
+   constructor(
+     commands: CommandRegistry,
+     options?: Cell.ContentFactory.IOptions | undefined
+   ) {
+     super(options);
+     this.commands = commands;
+   }
+   /**
+    * Create a new cell header for the parent widget.
+    */
+   createCellFooter(): ICellFooter {
+     return new CellFooterWithButton(this.commands);
+   }
+   private readonly commands: CommandRegistry;
+ }
+ 
+ /**
+  * Extend default implementation of a cell footer.
+  */
+ export class CellFooterWithButton extends ReactWidget implements ICellFooter {
+   /**
+    * Construct a new cell footer.
+    */
+   constructor(commands: CommandRegistry) {
+     super();
+     this.addClass(CELL_FOOTER_CLASS);
+     this.commands = commands;
+   }
+
+   private readonly commands: CommandRegistry;
+
+   render() {
+      console.log('Rendering element');
+      return React.createElement("div", {className: CELL_FOOTER_DIV_CLASS }, 
+        React.createElement("button",{
+            className: CELL_FOOTER_BUTTON_CLASS,
+            onClick: () => {
+              console.log("Clicked run cell");
+              this.commands.execute('run-selected-codecell');
+            },
+          },"pin"));
+  }
+ }
+
+/**
+ * The foot button extension for the code cell.
+ */
+ const footerButtonExtension: JupyterFrontEndPlugin<void> = {
+  id: 'jpnb_t1',
+  autoStart: true,
+  activate: activateCommands,
+  requires: [INotebookTracker]
+};
+/**
+ * The notebook cell factory provider.
+ */
+ const cellFactory: JupyterFrontEndPlugin<NotebookPanel.IContentFactory> = {
+  id: 'jpnb_t1:factory',
+  provides: NotebookPanel.IContentFactory,
+  requires: [IEditorServices],
+  autoStart: true,
+  activate: (app: JupyterFrontEnd, editorServices: IEditorServices) => {
+    // tslint:disable-next-line:no-console
+    console.log(
+      'JupyterLab extension jupyterlab-cellcodebtn',
+      'overrides default nootbook content factory'
+    );
+
+    const { commands } = app;
+    const editorFactory = editorServices.factoryService.newInlineEditor;
+    return new ContentFactoryWithFooterButton(commands, { editorFactory });
+  }
+};
 
 /**
  * The plugin registration information.
  */
-const plugin: JupyterFrontEndPlugin<void> = {
+ const two_dim_jup_plugin: JupyterFrontEndPlugin<void> = {
   activate,
   id: 'toolbar-button',
   autoStart: true,
 };
+
+/**
+ * Initialization data for the jpnb_t1 extension.
+ */
+const plugins: Array<JupyterFrontEndPlugin<any>> = [
+  footerButtonExtension,
+  cellFactory,
+  two_dim_jup_plugin
+]
+export default plugins;
 
 /**
  * A notebook widget extension that adds a button to the toolbar.
@@ -139,12 +327,12 @@ export class ButtonExtension
         newColumn.prepend(toolbar);
       }
 
-      const cellFactory = NBTestUtils.createCodeCellFactory();
-      const cellModel = new CodeCellModel({});
-      var newCodeCell = new CodeCell({model:cellModel, contentFactory:cellFactory, rendermime});
-      newCodeCell.setPrompt('');
-      newCodeCell.model.metadata.set("column", numColumns + 1);
-      $(newColumn).append(newCodeCell as unknown as HTMLDivElement);
+      // const cellFactory = NBTestUtils.createCodeCellFactory();
+      // const cellModel = new CodeCellModel({});
+      // var newCodeCell = new CodeCell({model:cellModel, contentFactory:cellFactory, rendermime });
+      // newCodeCell.setPrompt('');
+      // newCodeCell.model.metadata.set("column", numColumns + 1);
+      // $(newColumn).append(newCodeCell as unknown as HTMLDivElement);
     }
 
     const addColumnButton = new ToolbarButton({
@@ -214,54 +402,54 @@ function update_styling() {
   nCols = columns.length;
   if(nCols == 0) {
     nCols = 2; // default to at least 2
-  }
-  // var newColumnWidth = 100/nCols - 2;
-  var newNbContainerWidth = 900*nCols + 50;
-  
-  // Tries to get jp-Notebook page element, which there should be one and only one of 
-  var notebook: HTMLCollectionOf<Element> = document.getElementsByClassName("jp-Notebook");
-  if(notebook.length == 0) {
-    // Upon initial call, the jp-Notebook element likely has not loaded yet
-    console.log("Error: no jp-Notebook class elements found");
-    console.log(notebook);
-  } else {
-    // If jp-Notebook is found, add columns to it as appropriate
-    (notebook[0] as HTMLElement).style.width = newNbContainerWidth.toString() + "px";
-    // (notebook[0] as HTMLElement).style.backgroundColor = "#00ff00";
-    (notebook[0] as HTMLElement).style.overflowX = "visible";
-    (notebook[0] as HTMLElement).style.overflowY = "visible";
-    (notebook[0] as HTMLElement).style.height = 'inherit';
+    // var newColumnWidth = 100/nCols - 2;
+    var newNbContainerWidth = 900*nCols + 50;
+    
+    // Tries to get jp-Notebook page element, which there should be one and only one of 
+    var notebook: HTMLCollectionOf<Element> = document.getElementsByClassName("jp-Notebook");
+    if(notebook.length == 0) {
+      // Upon initial call, the jp-Notebook element likely has not loaded yet
+      console.log("Error: no jp-Notebook class elements found");
+      console.log(notebook);
+    } else {
+      // If jp-Notebook is found, add columns to it as appropriate
+      (notebook[0] as HTMLElement).style.width = newNbContainerWidth.toString() + "px";
+      // (notebook[0] as HTMLElement).style.backgroundColor = "#00ff00";
+      (notebook[0] as HTMLElement).style.overflowX = "visible";
+      (notebook[0] as HTMLElement).style.overflowY = "visible";
+      (notebook[0] as HTMLElement).style.height = 'inherit';
 
-    var new_columns = [];
-    for(var c = 0; c < nCols; c++){
-      new_columns[c] = document.createElement('div');   
-      new_columns[c].classList.add("column");
-      new_columns[c].id = "column" + (c+1).toString();
-      new_columns[c].style.width = "500px";
-      // colWidths[c] = newColumnWidth.toString() + "%";
-      new_columns[c].style.float = 'left';
-      new_columns[c].style.margin = "10px";
-      new_columns[c].style.height = "inherit";
-      new_columns[c].style.minHeight = "30px";
-      new_columns[c].style.backgroundColor = "rgba(255,255,255,0.5)";
+      var new_columns = [];
+      for(var c = 0; c < nCols; c++){
+        new_columns[c] = document.createElement('div');   
+        new_columns[c].classList.add("column");
+        new_columns[c].id = "column" + (c+1).toString();
+        new_columns[c].style.width = "500px";
+        // colWidths[c] = newColumnWidth.toString() + "%";
+        new_columns[c].style.float = 'left';
+        new_columns[c].style.margin = "10px";
+        new_columns[c].style.height = "inherit";
+        new_columns[c].style.minHeight = "30px";
+        new_columns[c].style.backgroundColor = "rgba(255,255,255,0.5)";
 
-      notebook[0].appendChild(new_columns[c]);
-      var colIndex = new_columns[c].id;
-      colIndex = colIndex.replace('column', '');
-      console.log("Made column");
-      console.log(new_columns[c].toString())
-      var toolbar = createColumnToolbar(parseInt(colIndex));
-      new_columns[c].prepend(toolbar);
-
+        notebook[0].appendChild(new_columns[c]);
+        var colIndex = new_columns[c].id;
+        colIndex = colIndex.replace('column', '');
+        console.log("Made column");
+        console.log(new_columns[c].toString())
+        var toolbar = createColumnToolbar(parseInt(colIndex));
+        new_columns[c].prepend(toolbar);
+      }
     }
+    var columns: HTMLCollectionOf<Element> = document.getElementsByClassName("column");
+    // Assume there are columns now
     // For now, assume one column, and we are completing initial page setup
     var jp_cells = (notebook[0] as HTMLElement).getElementsByClassName("jp-Cell");
     console.log(jp_cells.length)
     for(var i = 0; i < jp_cells.length; i++) {
-      new_columns[0].append(jp_cells[i]);
-      console.log("append attempted");
+      columns[0].append(jp_cells[i]);
+      console.log("append attempted: ", jp_cells[i]);
     }
-
   }
 }
 
@@ -377,7 +565,7 @@ function activate(app: JupyterFrontEnd): void {
   initialize()
 }
 
-/**
- * Export the plugin as default.
- */
-export default plugin;
+// /**
+//  * Export the plugin as default.
+//  */
+// export default plugin;
